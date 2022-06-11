613f38abc2ba3bcca53cd15cf240d3fbd1fedf36
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-06-11 15:42:00 -0400

    Developed the code to pass the purchases data from screen 1 to screen 2. Tested should works.

--- A2/app/src/main/java/com/seneca/a2_hien_hnguyen110/ListOfPurchasesActivity.java
+++ A2/app/src/main/java/com/seneca/a2_hien_hnguyen110/ListOfPurchasesActivity.java
@@ -2,17 +2,25 @@ package com.seneca.a2_hien_hnguyen110;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.seneca.a2_hien_hnguyen110.databinding.ActivityMainBinding;
+import com.seneca.a2_hien_hnguyen110.models.Purchase;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 
 public class ListOfPurchasesActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_list_of_purchases);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            Serializable serializablePurchases = intent.getSerializableExtra("EXTRA_PURCHASES");
+            ArrayList<Purchase> purchases = (ArrayList<Purchase>) serializablePurchases;
+        }
     }
 }

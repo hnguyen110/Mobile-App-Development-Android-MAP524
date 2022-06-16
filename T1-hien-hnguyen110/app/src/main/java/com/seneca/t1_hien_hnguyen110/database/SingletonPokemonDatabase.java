d7be68fc5c70a54ae0b3eaab3d4a5777dd1eb6e3
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-06-16 14:11:27 -0400

    Added the database class to share the state of the application (Pokemon data) to all the screens in the application. Added the function of getting the Pokemon information using the index number.

---
+++ T1-hien-hnguyen110/app/src/main/java/com/seneca/t1_hien_hnguyen110/database/SingletonPokemonDatabase.java
@@ -0,0 +1,39 @@
+package com.seneca.t1_hien_hnguyen110.database;
+
+import com.seneca.t1_hien_hnguyen110.models.Pokemon;
+
+import java.util.ArrayList;
+
+public class SingletonPokemonDatabase {
+    private static SingletonPokemonDatabase instance = null;
+    private ArrayList<Pokemon> pokemons = new ArrayList<>();
+
+    private SingletonPokemonDatabase() {
+        loadPokemonDataSet();
+    }
+
+    public static SingletonPokemonDatabase getInstance() {
+        if (instance == null) {
+            instance = new SingletonPokemonDatabase();
+        }
+        return instance;
+    }
+
+    public Pokemon getPokemonById(int pokedexNumber) {
+        Pokemon pokemon = null;
+        for (Pokemon record : pokemons) {
+            if (record.getPokedex() == pokedexNumber) {
+                pokemon = record;
+                break;
+            }
+        }
+        return pokemon;
+    }
+
+    private void loadPokemonDataSet() {
+        this.pokemons.add(new Pokemon(12, "Caterpie"));
+        this.pokemons.add(new Pokemon(19, "Rattata"));
+        this.pokemons.add(new Pokemon(25, "Pikachu"));
+        this.pokemons.add(new Pokemon(147, "Dratini"));
+    }
+}

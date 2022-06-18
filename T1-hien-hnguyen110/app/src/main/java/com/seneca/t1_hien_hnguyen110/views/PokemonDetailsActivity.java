d7a8d9f08cb24cffaf8d7a51e485ff8c0b56c597
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-06-17 20:09:10 -0400

    Added the options menu for the details screen

--- T1-hien-hnguyen110/app/src/main/java/com/seneca/t1_hien_hnguyen110/views/PokemonDetailsActivity.java
+++ T1-hien-hnguyen110/app/src/main/java/com/seneca/t1_hien_hnguyen110/views/PokemonDetailsActivity.java
@@ -1,9 +1,15 @@
 package com.seneca.t1_hien_hnguyen110.views;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.seneca.t1_hien_hnguyen110.R;
 import com.seneca.t1_hien_hnguyen110.databinding.ActivityPokemonDetailsBinding;
 
 public class PokemonDetailsActivity extends AppCompatActivity {
@@ -14,5 +20,25 @@ public class PokemonDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityPokemonDetailsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.pokemon_details_options_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.setAsFavouriteOption:
+                return true;
+            case R.id.resetOption:
+                return true;
+            default:
+                return true;
+        }
     }
 }

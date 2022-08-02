bb268d22292626ae3a4da69b7f67f6379a31daa4
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-08-02 14:23:32 -0400

    Created the Purchase entity

---
+++ Movies-hien-hnguyen110/app/src/main/java/com/seneca/movies_hien_hnguyen110/models/Purchase.java
@@ -0,0 +1,31 @@
+package com.seneca.movies_hien_hnguyen110.models;
+
+public class Purchase {
+    private int id;
+    private String title;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}

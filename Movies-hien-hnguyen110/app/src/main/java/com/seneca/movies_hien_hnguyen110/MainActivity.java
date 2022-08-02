acd11e7007fa2960e9ce61532f73178f97699956
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-08-02 19:00:49 -0400

    Added the bottom navigation bar to move around 2 screen

--- Movies-hien-hnguyen110/app/src/main/java/com/seneca/movies_hien_hnguyen110/MainActivity.java
+++ Movies-hien-hnguyen110/app/src/main/java/com/seneca/movies_hien_hnguyen110/MainActivity.java
@@ -3,6 +3,9 @@ package com.seneca.movies_hien_hnguyen110;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.seneca.movies_hien_hnguyen110.databinding.ActivityMainBinding;
 
@@ -12,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+            NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        }
     }
 }
